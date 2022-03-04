b8baf82c6e61a25f3eb314e03409126f288cdca6
Pranali <pranalilambat00@gmail.com>
2022-03-04 14:48:06 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Log_Algo;
+
+namespace DS_Algorithm_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the algorithm problems");
+           
+            //Insertion_Sort insertion = new Insertion_Sort();
+            //insertion.Sorting();
+
+
+        }
+    }
+}
